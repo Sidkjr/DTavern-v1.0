d66c17631c13b0a0424be400048b399bd0aef2c6
Sidkjr <siddhant.khare.sk@gmail.com>
2023-08-28 19:57:12 +0530

    Added Sample Thought Feed

--- src/components/TweetInFeed.js
+++ src/components/TweetInFeed.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './TweetInFeed.css';
+import golf from "../images/golf.png"
+import canoe from "../images/canoe.png"
+import { defaultImgs } from "../defaultimgs";
+import { Icon } from "web3uikit";
 
 
 const TweetInFeed = () => {
@@ -7,6 +11,56 @@ const TweetInFeed = () => {
 
   return (
     <>
+    <div className="feedTweet">
+      <img src={defaultImgs[0]} className="profilePic"></img>
+      <div className="completeTweet">
+        <div className="who">
+          Sid
+          <div className="accWhen">0x42..314 . 1h</div>
+        </div>
+        <div className="tweetContent">
+          Nice Day Golfing today Shot 73 (+2)
+          <img src={golf} className="tweetImg"></img>
+        </div>
+        <div className="interactions">
+          <div className="interactionNums">
+            <Icon fill="#3f3f3f" size={20} svg="messageCircle"></Icon>
+          </div>
+          <div className="interactionNums">
+            <Icon fill="#3f3f3f" size={20} svg="star"></Icon>
+          </div>
+          <div className="interactionNums">
+            <Icon fill="#3f3f3f" size={20} svg="matic"></Icon>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="feedTweet">
+      <img src={defaultImgs[0]} className="profilePic"></img>
+      <div className="completeTweet">
+        <div className="who">
+          Sujal
+          <div className="accWhen">0x71..539 . 1h</div>
+        </div>
+        <div className="tweetContent">
+          Going on a Canoe ride
+          <img src={canoe} className="tweetImg"></img>
+        </div>
+        <div className="interactions">
+          <div className="interactionNums">
+            <Icon fill="#3f3f3f" size={20} svg="messageCircle"></Icon>
+          </div>
+          <div className="interactionNums">
+            <Icon fill="#3f3f3f" size={20} svg="star"></Icon>
+          </div>
+          <div className="interactionNums">
+            <Icon fill="#3f3f3f" size={20} svg="matic"></Icon>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     </>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,23 @@ import React from "react";
 import { defaultImgs } from "../defaultimgs";
 import "./Home.css";
 import { Icon, TextArea } from "web3uikit";
+import { useState, useRef } from "react";
+import TweetInFeed from "../components/TweetInFeed";
 
 const Home = () => {
+
+  const inputFile = useRef(null);
+  const [selectedFile, setSelectedFile] = useState();
+
+  const onImageClick = () => {
+    inputFile.current.click();
+  };
+
+  const changeHandler = (event) => {
+    const img = event.target.files[0];
+    setSelectedFile(URL.createObjectURL(img));
+  };
+
   return (
     <>
       <div className="mainContent">
@@ -21,8 +36,16 @@ const Home = () => {
             width="95%">
 
             </TextArea>
+            {selectedFile && (
+              <img src={selectedFile} className="tweetImg"></img>
+            )}
             <div className="imgOrTweet">
-              <div className="imgDiv">
+              <div className="imgDiv" onClick={onImageClick}>
+                <input type="file"
+                        name="file"
+                        ref={inputFile}
+                        onChange={changeHandler}
+                        style={{ display: "none"}}/>
                 <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
               </div>
               <div className="tweetOptions">
@@ -34,6 +57,7 @@ const Home = () => {
             </div>
           </div>
         </div>
+        <TweetInFeed profile={false}/>
       </div>
     </>
   );
